5891edf769f59e8f28146441a103ee23455595c0
Netra Pradhan <npradha@purdue.edu>
2018-12-22 08:39:58 -0600

    Practice using Random module

--- hangman.py
+++ hangman.py
@@ -1,3 +1,17 @@
 import random
 
+colors = ['red', 'orange']
+animals = ['dog', 'cat']
+states = ['texas', 'arkansas']
+allLists = [colors, animals, states]
+
+colorOption = random.choice(colors)
+print(colorOption)
+print(len(colorOption))
+
+print(random.choice(animals))
+print(random.choice(states))
+
+randomOption = random.choice(allLists)
+print(random.choice(randomOption))
 
